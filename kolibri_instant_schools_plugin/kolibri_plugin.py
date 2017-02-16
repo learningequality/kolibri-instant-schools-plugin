6fe498c8138a3b964998306903a053c2a72b0100
Richard Tibbles <richard.tibbles@gmail.com>
2017-02-15 16:21:09 -0800

    Don't import hooks and urls that don't exist.

--- kolibri_instant_schools_plugin/kolibri_plugin.py
+++ kolibri_instant_schools_plugin/kolibri_plugin.py
@@ -1,7 +1,4 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from kolibri.core.webpack import hooks as webpack_hooks
 from kolibri.plugins.base import KolibriPluginBase
-from . import hooks, urls
-
-
 

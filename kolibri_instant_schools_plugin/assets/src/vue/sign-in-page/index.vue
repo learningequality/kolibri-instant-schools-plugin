640d3ddb6636167153086468a4b6ac5f58220885
66eli77 <66eli77@gmail.com>
2017-02-16 18:09:25 -0800

    style signin page, import styles from local theme.styl file for now.

--- kolibri_instant_schools_plugin/assets/src/vue/sign-in-page/index.vue
+++ kolibri_instant_schools_plugin/assets/src/vue/sign-in-page/index.vue
@@ -1,44 +1,55 @@
 <template>
 
-  <div>
-    <img class="logo" alt="logo">
-    <h1>{{ $tr('signIn') }}</h1>
+  <div class="login">
+    <div id="login-container">
+      <br>
+      <br>
+      <img class="logo" src="../img/instant-school-logo.png" alt="logo">
+      <h1 class="login-text title">{{ $tr('instantSchool') }}</h1>
+      <br>
+      <form ref="form" @submit.prevent="signIn">
+        <ui-autocomplete
+          :label="$tr('username')"
+          id="username"
+          type="text"
+          :placeholder="$tr('enterUsername')"
+          :aria-label="$tr('username')"
+          v-model="username"
+          autocomplete="username"
+          required
+          autofocus/>
+        <br>
+        <ui-autocomplete
+          :label="$tr('password')"
+          id="password"
+          type="password"
+          :placeholder="$tr('enterPassword')"
+          :aria-label="$tr('password')"
+          v-model="password"
+          autocomplete="current-password"
+          required/>
+        <br>
+        <icon-button id="login-btn" :text="$tr('signIn')" :primary="true" type="submit"></icon-button>
 
-    <form ref="form" @submit.prevent="signIn">
-      <label for="username">{{ $tr('username') }}</label>
-      <input
-        id="username"
-        type="text"
-        :placeholder="$tr('enterUsername')"
-        :aria-label="$tr('username')"
-        v-model="username"
-        autocomplete="username"
-        required
-        autofocus>
-      <label for="password">{{ $tr('password') }}</label>
-      <input
-        id="password"
-        type="password"
-        :placeholder="$tr('enterPassword')"
-        :aria-label="$tr('password')"
-        v-model="password"
-        autocomplete="current-password"
-        required>
-      <icon-button :text="$tr('signIn')" :primary="true" type="submit"></icon-button>
+        <p v-if="loginError" class="sign-in-error">{{ $tr('signInError') }}</p>
+      </form>
+      <br>
+      <router-link class="login-text" id="password-reset" :to="signUp">{{ $tr('resetPassword') }}</router-link>
+      <br>
+      <div id="divid-line"></div>
 
-      <p v-if="loginError" class="sign-in-error">{{ $tr('signInError') }}</p>
-    </form>
-
-    <hr>
-
-    <p>{{ $tr('noAccount') }}</p>
-    <router-link :to="signUp">
-      <icon-button :text="$tr('createAccount')" :primary="true"></icon-button>
-    </router-link>
-
-    <a href="/">
-      <icon-button :text="$tr('accessAsGuest')" :primary="false"></icon-button>
-    </a>
+      <p class="login-text">{{ $tr('noAccount') }}</p>
+      <br>
+      <div id="btn-group">
+        <router-link class="group-btn" :to="signUp">
+          <icon-button :text="$tr('createAccount')" :primary="true"></icon-button>
+        </router-link>
+        <a class="group-btn" href="/">
+          <icon-button :text="$tr('accessAsGuest')" :primary="false"></icon-button>
+        </a>
+      </div>
+      <br>
+    </div>
   </div>
 
 </template>
@@ -52,18 +63,21 @@
   module.exports = {
     $trNameSpace: 'signInPage',
     $trs: {
-      signIn: 'Sign In',
-      username: 'Username',
-      enterUsername: 'Enter Username',
+      instantSchool: 'INSTANT SCHOOLS',
+      signIn: 'Log In',
+      username: 'Phone Number',
+      enterUsername: 'Enter phone number',
       password: 'Password',
       enterPassword: 'Enter Password',
       noAccount: `Don't have an account?`,
       createAccount: 'Create Account',
       accessAsGuest: 'Access as Guest',
       signInError: 'Incorrect username or password',
+      resetPassword: 'Reset your password',
     },
     components: {
       'icon-button': require('kolibri.coreVue.components.iconButton'),
+      'ui-autocomplete': require('keen-ui/src/UiAutocomplete'),
     },
     data: () => ({
       username: '',
@@ -97,18 +111,72 @@
 
 <style lang="stylus" scoped>
 
-  @require '~kolibri.styles.definitions'
+  @require '../../styles/theme.styl'
 
-  input
+  .login
+    background-color: $login-overlay
+    height: 100vh
+    background: url(../img/instant-background.jpg) no-repeat center center fixed
+    -webkit-background-size: cover
+    -moz-background-size: cover
+    -o-background-size: cover
+    background-size: cover
+    overflow-y: auto
+    overflow-x: hidden
+
+  #login-container
     display: block
+    margin: auto
+
+  .login-text
+    color: $login-text
+
+  .title
+    font-weight: 100
+
+  #login-btn
+    display:block
+    margin: auto
+    width: 100%
+
+  #btn-group
+    display:table
+    margin: auto
+
+  .group-btn
+    padding: 5px
+
+  #password-reset
+    display: table
+    margin: auto
+    font-size: 0.8em
+
+  #divid-line
+    width: 412px
+    height: 1px
+    background-color: $core-text-annotation
+    margin: auto
+
+  h1
+    font-size: 1.3em
+    letter-spacing: 0.1em
+
+  h1, p
+    text-align: center
+
+  img
+    position: relative
+    display: block
+    margin: auto
+    width: 16vh
+
+  form
+    width: 70%;
+    max-width: 300px;
+    margin: auto
 
   .sign-in-error
     color: red
 
-  input
-    display: block
-
-  .sign-in-error
-    color: red
 
 </style>

6fdaba6722dad7f478cbd3edd0fa7d70d6b088db
indirectlylit <devon@indirectlylit.com>
2017-02-16 22:39:30 -0800

    disable attempted overriding of styles by plugin

--- kolibri_instant_schools_plugin/assets/src/coreAPI.js
+++ kolibri_instant_schools_plugin/assets/src/coreAPI.js
@@ -1,12 +1,12 @@
 
 module.exports = {
-  styles: {
-    theme: {
-      module: require('./styles/theme.styl'),
-    },
-    keenVars: {
-      module: require('./styles/keen.scss'),
-    },
-  },
+  // styles: {
+  //   theme: {
+  //     module: require('./styles/theme.styl'),
+  //   },
+  //   keenVars: {
+  //     module: require('./styles/keen.scss'),
+  //   },
+  // },
 };
 

e83f61e3812c7af527ef863147c82f9a2805cb74
Jamie Alexandre <jamalex@gmail.com>
2017-02-26 20:07:50 -0800

    Added phone hashing utility functions.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         str(plugin_name),  # https://github.com/pypa/setuptools/pull/597
     ],
     package_dir={plugin_name: plugin_name},
+    install_requires=["backports.functools_lru_cache==1.2.1"],
     include_package_data=True,
     license='All Rights Reserved',
     zip_safe=False,

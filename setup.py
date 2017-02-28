161161ee9ccfc72c45807307067f9713eafdb28d
indirectlylit <devon@indirectlylit.com>
2017-02-28 10:58:43 -0800

    increment version number

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def enable_log_to_stdout(logname):
 
 setup(
     name=dist_name,
-    version="0.1",
+    version="0.2",
     description=description,
     long_description="{readme}\n\n{doclink}".format(
         readme=readme,

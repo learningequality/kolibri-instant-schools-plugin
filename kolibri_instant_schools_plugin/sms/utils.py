5c411f3bbff5cf586315798505409fe8cf50a767
Jacob Pierce <w.jacobpierce@gmail.com>
2022-08-19 13:58:10 -0700

    replace smpp w/ twilio, rename pkg smpp->sms

---
+++ kolibri_instant_schools_plugin/sms/utils.py
@@ -0,0 +1,41 @@
+import os
+from twilio.rest import Client
+from django.core.urlresolvers import reverse
+
+from ..auth.mapping import normalize_phone_number
+
+COUNTRY_PHONE_PREFIX = os.environ["COUNTRY_PHONE_PREFIX"]
+SMS_MESSAGE_TEMPLATE = os.environ["SMS_MESSAGE_TEMPLATE"]
+TWILIO_SID = os.environ["TWILIO_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+
+
+def send_password_reset_link(phone, token, baseurl):
+
+    phone = normalize_phone_number(phone)
+    baseurl = baseurl.rstrip("/")
+    endpointurl = reverse("kolibri:kolibri_instant_schools_plugin:passwordchange_list")
+    path = "{endpointurl}{phone}/{token}".format(endpointurl=endpointurl, token=token, phone=phone)
+
+
+    url = "{baseurl}/{path}".format(
+        baseurl=baseurl, path=path
+    )
+
+    send_message(phone, SMS_MESSAGE_TEMPLATE.format(url=url))
+
+
+def send_message(phone, message):
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    if not phone.startswith(COUNTRY_PHONE_PREFIX):
+        phone = COUNTRY_PHONE_PREFIX + phone
+
+    message = client.messages.create(
+      messaging_service_sid='MG5789fbd6bb5607f3c1a10a020bb11ca3',
+      to=phone,
+      body=message,
+    )
+
+    print(message.sid)
